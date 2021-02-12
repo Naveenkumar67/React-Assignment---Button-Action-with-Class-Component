f966409fa13209d81e28dd3dcbc3330c7ad691e9
Naveenkumar67 <64203146+Naveenkumar67@users.noreply.github.com>
2021-02-12 17:53:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,23 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
-
 class App extends Component {
     constructor(props) {
 		super(props);
+		this.show = this.show.bind(this);
+		this.state={
+			showMessage:false,
+		};
 	};
-handle(){
-	return(<p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
-	       )
-}
+
+	show(){
+		this.setState({showMessage:true,});
+	}
+   
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
-		<button onclick={this.handle}>Paragraph</button>
+				<button id="click" onClick={this.show}>clickToRender</button>
+				{this.state.showMessage && <p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
     		</div>
     	);
     }
 }
-
-
 export default App;
-
