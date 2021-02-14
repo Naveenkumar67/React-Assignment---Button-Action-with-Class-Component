a776a8cbb6f5431d8a6db53ad12fe419ccbadfc0
Naveenkumar67 <64203146+Naveenkumar67@users.noreply.github.com>
2021-02-14 19:17:11 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,37 @@
-import React, { Component } from "react";
-import '../styles/App.css';
-class App extends Component {
-    constructor(props) {
-		super(props);
-		this.show = this.show.bind(this);
-		this.state={
-			showMessage:false,
-		};
-	};
+  
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-	show(){
-		this.setState({showMessage:true,});
-	}
-   
-    render() {
-    	return(
-    		<div id="main">
-				<button id="click" onClick={this.show}>clickToRender</button>
-				{this.state.showMessage && <p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
-    		</div>
-    	);
-    }
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.show = this.show.bind(this);
+    this.state = {
+      showMessage: false,
+    };
+  }
+
+  show() {
+    this.setState({ showMessage: true });
+  }
+
+  render() {
+    return (
+      <div id="main">
+        {/* Do not remove this main div!! */}
+        <button id="click" onClick={this.show}>
+          Open
+        </button>
+        {this.state.showMessage && (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+          </p>
+        )}
+      </div>
+    );
+  }
 }
+
 export default App;
